01f5bc6d13f215fdc9c113b7d12a43c78e8579d7
James Eunson <james.eunson@gmail.com>
2023-01-07 15:59:40 +1100

    Initial Commit

---
+++ SimpleHN-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SimpleHN-SwiftUI
+//
+//  Created by James Eunson on 7/1/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleHN-SwiftUI/SimpleHN_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleHN_SwiftUIApp.swift
+//  SimpleHN-SwiftUI
+//
+//  Created by James Eunson on 7/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleHN_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
